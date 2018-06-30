34af8f6462c99821f03ca3c631b9e3a0a946b58e
Dajing <gongjean@yahoo.com>
2018-06-30 11:42:28 -0700

    Add display for percentage

---
+++ src/components/DisplayBrightness.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Sun from "react-icons/lib/fa/sun-o";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Container = styled.div`
+  text-align: center;
+  color: white;
+`;
+
+const StyleSun = styled(Sun)`
+  color: yellow;
+  font-size: 20px;
+`;
+
+export default class DisplayBrightness extends Component {
+  render() {
+    return (
+      <Container {...this.props}>
+        <StyleSun />
+        <h1> {this.props.percentage}% </h1>
+        <p> Brightness </p>
+      </Container>
+    );
+  }
+}
+
+DisplayBrightness.PropTypes = {
+  percentage: PropTypes.isNumber
+};
+
+DisplayBrightness.defaultProps = {
+  percentage: 0
+};
